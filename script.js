65441ea2246bf075b56f6d474d23e3e60ead371e
Edgar <edgarcrd@gmail.com>
2026-02-09 20:25:00 -0500

    Functionality Completed. Missing Styling

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const crypto = require('crypto');
+
 const myLibrary = [
           // {
           //   title: 'book1',
@@ -13,90 +15,107 @@ const myLibrary = [
           // }
         ];
 
-function Book(title, author, pages, read) {
+function Book (id, title, author, pages, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  console.log(pages)
+const bookcase = document.querySelector('.bookcase')
 
-  const newBook = {title: title, author: author, pages: pages, read: read}
-
+function addBookToLibrary(id, title, author, pages, read) {
+  var newBook = new Book(id, title, author, pages, read)
   myLibrary.push(newBook)
 
-
-  //MAKE THIS ONE A FUNCTION AND ADD IT TO "TEST" FUNCTION
   const card = document.createElement('div')
   card.classList.add('card')
+
   for (key in newBook) {
-    const book = document.createElement('div')
-    book.classList.add(key)
-    book.textContent = newBook[`${key}`]
-    card.appendChild(book);
-  }
-  bookcase.appendChild(card);
-
-  console.log(myLibrary)
-}
-
-const bookcase = document.querySelector('.bookcase')
-// const card = document.createElement('div')
-// card.textContent = 'Hello'
-
-
-const test = (array) => {
-  array.forEach(element => {
-    const card = document.createElement('div')
-    card.classList.add('card')
-    for (key in element) {
+    if (key !== 'id' && key !== 'read') {
       const book = document.createElement('div')
-      book.classList.add(`${key}`)
-      book.textContent = element[`${key}`]
+      book.classList.add(key)
+      book.textContent = newBook[`${key}`]
       card.appendChild(book);
     }
-    bookcase.appendChild(card);
-  });
+
+    if (key === "read") {
+      const book = document.createElement('button')
+      book.classList.add(key)
+
+      if (newBook[`${key}`] === true) {
+        book.textContent = 'Read'
+      } else {
+        book.textContent = 'Unread'
+      }
+
+      book.addEventListener('click', () => {
+        const e = document.getElementById(id);
+        const pos = myLibrary.map(e => e.id).indexOf(id);
+        if (book.textContent === 'Unread') {
+          book.textContent = "Read";
+          myLibrary[pos]["read"] = true
+        } else {
+          book.textContent = "Unread";
+          myLibrary[pos]["read"] = false
+        }
+      })
+
+      card.appendChild(book);
+    }
+  }
+
+  const delBtn = document.createElement('button')
+  delBtn.id = id;
+  delBtn.classList.add("del-btn")
+  delBtn.textContent = "delete"
+
+  delBtn.addEventListener('click', () => {
+    const elem = document.getElementById(delBtn.id);
+    const pos = myLibrary.map(e => e.id).indexOf(delBtn.id);
+    elem.parentElement.remove();
+    myLibrary.splice(pos, 1);
+  })
+
+  card.appendChild(delBtn);
+  bookcase.appendChild(card);
 }
 
 const form = document.getElementById('newBookForm');
 
 form.addEventListener('submit', (e) => {
-
-
-  console.log('hey')
   // Prevent the default form submission (page reload)
   e.preventDefault();
-
-  // const formData = new FormData(form);
-
-  // const title = formData.get('title');
-
-  // document.getElementById("read").checked = true;
-
-
-  //FIGURE OUT CHECKBOX
 
   const title = document.getElementById("title")
   const author = document.getElementById('author');
   const pages = document.getElementById('pages');
   const read = document.getElementById('read');
 
-  addBookToLibrary(title.value, author.value, pages.value, read.checked);
+  const uuid = window.crypto.randomUUID();
 
-  // title.value = "";
-  // author.value = "";
-  // pages.value = "";
-  // read.value = "";
+  addBookToLibrary(uuid, title.value, author.value, pages.value, read.checked);
 
   form.reset();
 
-  // test(myLibrary)
 });
 
-// addBookToLibrary("Mi Vida", "Edgar J", 120, false);
+// const test = (array) => {
+//   array.forEach(element => {
+//     const card = document.createElement('div')
+//     card.classList.add('card')
+//     for (key in element) {
+//       const book = document.createElement('div')
+//       book.classList.add(`${key}`)
+//       book.textContent = element[`${key}`]
+//       card.appendChild(book);
+//     }
+//     bookcase.appendChild(card);
+//   });
+// }
 
-// test(myLibrary)
+
+
+
 
